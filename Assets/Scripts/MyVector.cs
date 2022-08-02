b00feeb6f6b0c4bd65ef84aaedef1b28bd4d892b
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-08-02 07:44:29 -0500

    funciona
    a

---
+++ Assets/Scripts/MyVector.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct MyVector
+{
+    public float x;
+    public float y;
+
+    public MyVector(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public MyVector Sum(MyVector other)
+    {
+        return new MyVector(
+            x + other.x,
+            y + other.y
+        );
+    }
+    public MyVector sub(MyVector other)
+    {
+        return new MyVector(
+            x - other.x,
+            y - other.y
+        );
+    }
+    public MyVector Scale(float other)
+    {
+        return new MyVector(
+            x * other,
+            y * other
+        );
+    }
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return a.Sum(b);
+    }
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return a.sub(b);
+    }
+    public static MyVector operator *(MyVector a, float b)
+    {
+        return a.Scale(b);
+    }
+    public static MyVector operator *(float b, MyVector a)
+    {
+        return a.Scale(b);
+    }
+    public void Draw(Color color)
+    {
+        Debug.DrawLine(
+            new Vector3(0, 0, 0),
+            new Vector3(x, y, 0),
+            color,
+            0
+        );
+    }
+    public static explicit  operator Vector3(MyVector a)
+    {
+        return new Vector3(a.x,a.y,0);
+    }
+    public MyVector Lerp(MyVector a,MyVector b, float c)
+    {
+        return new MyVector(
+            (a.x+(a.x-b.x)*c),
+            (a.x+(a.y -b.y)*c)
+            );
+    }
+    public void Draw(MyVector origin,Color color)
+    {
+        Debug.DrawLine(
+            new Vector3(origin.x,origin.y,0),
+            new Vector3(x+origin.x, y+origin.y, 0),
+            color,
+            0
+        );
+    }
+}
---
+++ Assets/Scripts/TestVector.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVector : MonoBehaviour
+{
+
+    [SerializeField]
+    private MyVector myFirstVector;
+
+    [SerializeField]
+    private MyVector mySecondVector;
+    [SerializeField]
+    private MyVector mySecondVectorV1;
+    [SerializeField]
+    private MyVector mySecondVectorV2;
+    [Range(-1f, 1f), SerializeField] float relativeDistance;
+ 
+    void Start()
+    {/*
+        //MyVector obj = new MyVector(5,5);
+        MyVector sumResult = myFirstVector + mySecondVector;
+        myFirstVector.Sum(mySecondVector);
+        Debug.Log(sumResult.x);
+        Debug.Log(sumResult.y);
+
+        MyVector resResult = myFirstVector - mySecondVector;
+        myFirstVector.sub(mySecondVector);
+        Debug.Log(resResult.x);
+        Debug.Log(sumResult.y);
+
+        MyVector relsultMult = myFirstVector * 2;
+        */
+        //Vector2 a = Vector2.left;
+        //Vector2 b = Vector2.down;
+        //Vector2 c = a + b;
+        MyVector other = new MyVector(5,4);
+        Debug.DrawLine((Vector3)other,Vector3.down,Color.red);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        mySecondVectorV1.Draw(Color.blue);
+        mySecondVectorV2.Draw(Color.red);
+
+        MyVector diff = (mySecondVectorV2 - mySecondVectorV1)* relativeDistance;
+        diff.Draw(mySecondVectorV1,Color.yellow);
+        MyVector lerp = (mySecondVectorV1 + diff);
+        lerp.Draw(Color.green);
+    }
+}
